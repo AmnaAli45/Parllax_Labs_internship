4b4af1bfce86c7e7d8d43f8965b6e971ea84379d
AmnaAli45 <anasahmed.ru6@gmail.com>
2026-09-20 12:28:04 +0500

    function for unicode normalization created

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,4 +5,9 @@ import re
 def html_stripping(text):
     clean_text = re.sub(re.compile('<.*?>'),text)
     return clean_text
-    
+
+
+# Unicode Normalization
+def unicode_normalization(text):
+    clean_text = text.encode('utf_8')
+    return clean_text
