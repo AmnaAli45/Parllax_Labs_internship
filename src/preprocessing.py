b31d798dcf7ca5aed45e88a8d3a1618b779085a2
AmnaAli45 <anasahmed.ru6@gmail.com>
2026-09-20 12:49:28 +0500

    function to remove whitespaces created

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -11,3 +11,12 @@ def html_stripping(text):
 def unicode_normalization(text):
     clean_text = text.encode('utf_8')
     return clean_text
+
+# WhiteSpace Removal
+def remove_whitespaces(text):
+    clean_text = text.replace("\xa0"," ") # normal space
+    clean_text = clean_text.replace("\r\n","\n") #line endings -> \n
+    clean_text = re.sub(r"[\t]+", " ", clean_text) #tabs handling
+    clean_text = re.sub(r" ?\n ?", "\n", clean_text)  # trim spaces around newlines
+    clean_text = re.sub(r"\n{3,}", "\n\n", clean_text) #max one blank line between paragraphs
+    return clean_text.strip()  
